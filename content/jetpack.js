d8887d58e2bcde3c2c95c83bb6a8545ec79a32ed
Jeff Griffiths <jeff@canuckistani.ca>
2011-06-09 20:36:47 -0700

    initial commit of the scaffolded Komodo extension

---
+++ content/jetpack.js
@@ -0,0 +1,34 @@
+/**
+ * namespace Jetpack
+ *
+ * A JavaScript api for Komodo for driving the Firefox addons SDK command-line
+   tool 'cfx'
+ */
+
+if(typeof(Jetpack) == 'undefined') {
+    var Jetpack = {};
+}
+
+(function() {
+
+/**
+ * run the extension's tests.
+ */
+this.test = function() {}
+
+/**
+ * run the extension
+ */
+this.run = function() {}
+
+/**
+ * build an xpi for this extension
+ */
+this.build = function() {}
+
+/**
+ * open the docs in a Komodo tab, or (? pref ?) in a new browser tab
+ */
+this.docs = function() {}
+
+}).apply(Jetpack);
